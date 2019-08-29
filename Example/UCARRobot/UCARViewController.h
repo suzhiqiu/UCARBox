f9339bea3fb697df82d84dd9e0ffc27bb84ed281
suzhiqiu <zhiqiu.su@ucarinc.com>
2019-08-29 11:36:48 +0800

    添加路径

---
+++ Example/UCARRobot/UCARViewController.h
@@ -0,0 +1,13 @@
+//
+//  UCARViewController.h
+//  UCARRobot
+//
+//  Created by 364121248@qq.com on 08/29/2019.
+//  Copyright (c) 2019 364121248@qq.com. All rights reserved.
+//
+
+@import UIKit;
+
+@interface UCARViewController : UIViewController
+
+@end
---
+++ Example/UCARRobot/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  UCARRobot
+//
+//  Created by 364121248@qq.com on 08/29/2019.
+//  Copyright (c) 2019 364121248@qq.com. All rights reserved.
+//
+
+@import UIKit;
+#import "UCARAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([UCARAppDelegate class]));
+    }
+}
